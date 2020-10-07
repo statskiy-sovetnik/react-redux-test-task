864e3e893b334aafac41dc3f6a332d43f400aa72
Ivan Gladkikh <ivangladkikh3@gmail.com>
2020-10-07 17:55:50 +0500

    (add): First commit
    Added index.js and a single sample component

---
+++ src/scripts/Comp.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class Comp extends React.Component {
+    render(){
+        return (
+          <div>
+              Comp!
+          </div>
+        );
+    }
+}
+
+export {Comp};
