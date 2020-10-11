6ce9c81f635aa08e73829d8a13e9591f128dcd3b
Ivan Gladkikh <ivangladkikh3@gmail.com>
2020-10-11 21:08:56 +0500

    (add): Added build directory and gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -22,6 +22,39 @@ gulp.task("bundle-js", () => {
         .pipe(gulp.dest("src/dist"));
 });
 
+/*__________ BUILD _____________________________*/
+
+gulp.task("build-public", function () {
+    return gulp.src("public/*")
+        .pipe(gulp.dest("build/public"));
+});
+
+gulp.task("build-js", function () {
+    return gulp.src("src/dist/index.js")
+        .pipe(gulp.dest("build/src/dist/"));
+});
+
+gulp.task("build-css", function () {
+    return gulp.src("src/css/*.css")
+        .pipe(gulp.dest("build/src/css/"));
+});
+
+gulp.task("build-fonts", () => {
+    return gulp.src("src/fonts/*")
+        .pipe(gulp.dest("build/src/fonts"))
+})
+
+gulp.task('build', gulp.series(
+    ["gulp-sass",
+        "bundle-js",
+        "build-js",
+        "build-css",
+        "build-fonts",
+        "build-public"]
+));
+
+//__________________________________________________
+
 gulp.task("watch", () => {
     gulp.watch(["src/sass/**/*.sass", "src/scripts/**/*.js", "src/index.js"], gulp.series("gulp-sass", "bundle-js"));
 });
